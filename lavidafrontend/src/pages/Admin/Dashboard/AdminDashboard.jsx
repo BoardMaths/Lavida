820b367e23334ea462fea5b88731a46cc26e3acc
BoardMaths <lvic3690@gmail.com>
2024-12-05 13:38:46 +0100

    Add files via upload

---
+++ lavidafrontend/src/pages/Admin/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideBar from "./SideBar/SideBar.jsx";
+import Main from "./Main/Main.jsx";
+
+const AdminDashboard = () => {
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+  );
+};
+
+export default AdminDashboard;
